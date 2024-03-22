ddc310348fc3ea5ce64f7e1a67b1ec7b8ea90b21
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2024-03-22 09:53:49 -0300

    refactor: configura build do projeto

--- post-build.js
+++ post-build.js
@@ -1,5 +1,17 @@
 import fs from "fs";
 
+function getAllFilesInsideDir(dir, filesToCopy = []) {
+  const files = fs.readdirSync(dir);
+  files.forEach((file) => {
+    if (fs.lstatSync(`${dir}/${file}`).isDirectory()) {
+      const result = getAllFilesInsideDir(`${dir}/${file}`);
+      filesToCopy.push(...result);
+      return;
+    }
+    filesToCopy.push(`${dir}/${file}`);
+  });
+  return filesToCopy;
+}
 
 const envProduction = fs.readFileSync("./.env.production", "utf8");
 let viteApiUrl = envProduction.split("\n").find((line) => line.startsWith("VITE_API_URL"));
@@ -23,3 +35,19 @@ content = content.replace(/"/g, '\\"');
 const appFile = fs.readFileSync("public/app.js", "utf8");
 const appFileContent = appFile.replace("/** TEMPLATE DATA **/", content);
 fs.writeFileSync("dist/app.js", appFileContent);
+
+// copy directory and all files from ./dist to ../system/public/chat
+if (fs.existsSync("../system/public/chat")) {
+  fs.rmSync("../system/public/chat", { recursive: true });
+}
+fs.mkdirSync("../system/public/chat/assets", { recursive: true, mode: 0o755 });
+
+const filesToCopy = getAllFilesInsideDir("dist");
+filesToCopy.forEach((file) => {
+  const finalFile = file.replace(/^dist\//, '');
+  if (fs.lstatSync(file).isDirectory()) {
+    fs.mkdirSync(`../system/public/chat/${finalFile}`, { recursive: true, mode: 0o755 });
+    return;
+  }
+  fs.copyFileSync(file, `../system/public/chat/${finalFile}`);
+});
